46a60cc28938f577f2c3f4c766e5b6f093a7dbcf
Kliment <kliment1986@inbox.ru>
2017-11-11 23:57:37 +0300

    структура проекта

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux/Aux';
+
+class BurgerBuilder extends Component {
+
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;

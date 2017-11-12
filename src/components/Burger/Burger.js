49353f29f30c164b97aa9b2284a4e77d880aa9c7
Kliment <kliment1986@inbox.ru>
2017-11-12 11:36:30 +0300

    Добавил проверку на отсутствие ингридиентов

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,17 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import { ingredientsList } from './BurgerIngredient/ingredients.const';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, index) => {
-      return <BurgerIngredient ingredient={igKey} key={igKey + index} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient ingredient={igKey} key={igKey + index} />;
+      });
+    })
+    .reduce((arr, el) => arr.concat(el), []);
 
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Добавьте ингридиенты!</p>;
+  }
   console.log(transformedIngredients);
 
   return (
